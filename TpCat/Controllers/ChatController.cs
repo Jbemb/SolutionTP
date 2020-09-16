0199c89acc8fb13bfc0b8b491f9359c8049926ae
Janet BEMBNISTER <janet.bembnister2020@campus-eni.fr>
2020-09-16 15:32:55 +0200

    cats

---
+++ TpCat/Controllers/ChatController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TpCat.Data;
+using TpCat.Models;
+
+namespace TpCat.Controllers
+{
+    public class ChatController : Controller
+    {
+        // GET: Chat
+        public ActionResult Index()
+        {
+            List<Chat> CatList = FakeDb.Instance.ListeChats;
+          
+            return View(CatList);
+        }
+
+        // GET: Chat/Details/5
+        public ActionResult Details(int id)
+        {
+            Chat cat = FakeDb.Instance.ListeChats.FirstOrDefault(x => x.Id ==id);
+            return View(cat);
+        }
+
+        // GET: Chat/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Chat cat = FakeDb.Instance.ListeChats.FirstOrDefault(x => x.Id == id);
+            return View(cat);
+        }
+
+        // POST: Chat/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                Chat cat = FakeDb.Instance.ListeChats.FirstOrDefault(x => x.Id == id);
+                FakeDb.Instance.ListeChats.Remove(cat);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ TpCat/Data/FakeDb.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TpCat.Models;
+
+namespace TpCat.Data
+{
+    public class FakeDb
+    {
+        private static readonly Lazy<FakeDb> lazy =
+           new Lazy<FakeDb>(() => new FakeDb());
+
+        public static FakeDb Instance { get { return lazy.Value; } }
+
+        private FakeDb()
+        {
+           
+            this.ListeChats = new List<Chat>();
+            this.InitializeData();
+
+        }
+
+        public List<Chat> ListeChats { get; private set; }
+
+        public void InitializeData()
+        {
+            var i = 1;
+            ListeChats.Add(new Chat { Id = i++, Nom = "Felix", Age = 3, Couleur = "Roux" });
+            ListeChats.Add(new Chat { Id = i++, Nom = "Minette", Age = 1, Couleur = "Noire" });
+            ListeChats.Add(new Chat { Id = i++, Nom = "Miss", Age = 10, Couleur = "Blanche" });
+            ListeChats.Add(new Chat { Id = i++, Nom = "Garfield", Age = 6, Couleur = "Gris" });
+            ListeChats.Add(new Chat { Id = i++, Nom = "Chatran", Age = 4, Couleur = "Fauve" });
+            ListeChats.Add(new Chat { Id = i++, Nom = "Minou", Age = 2, Couleur = "Blanc" });
+            ListeChats.Add(new Chat { Id = i, Nom = "Bichette", Age = 12, Couleur = "Rousse" });   
+            
+        }
+    }
+}
